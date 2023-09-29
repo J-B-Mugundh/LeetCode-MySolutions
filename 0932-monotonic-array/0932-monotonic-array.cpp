36b038985145742d9cb0b6891448761d1de33925
Mugundh J B <mugundhjb@gmail.com>
2023-09-29 10:59:03 +0530

    Time: 134 ms (20.56%), Space: 96.7 MB (22.05%) - LeetHub

--- 0932-monotonic-array/0932-monotonic-array.cpp
+++ 0932-monotonic-array/0932-monotonic-array.cpp
@@ -18,9 +18,10 @@ public:
     // }
 
     bool isMonotonic(vector<int>& A) {
+        int n = A.size();
         bool increase = true;
         bool decrease = true;
-        for(int i = 0; i < A.size() - 1; i++) {
+        for(int i = 0; i < n - 1; i++) {
             if(A[i] > A[i+1]) increase = false;
             if(A[i] < A[i+1]) decrease = false;
             if(increase == false && decrease == false) return false;
