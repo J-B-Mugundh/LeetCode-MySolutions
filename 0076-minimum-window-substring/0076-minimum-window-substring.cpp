a149d98417c87e30128b4394af0fc52a67a660a5
Mugundh J B <mugundhjb@gmail.com>
2024-02-04 20:25:20 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,20 +1,38 @@
 class Solution {
 public:
-        string minWindow(string s, string t) {
-        unordered_map<char, int> letters; //unordered map for storing the characters in t that we need to check for in s
-        for(auto c : t) letters[c]++; 
-        int count = 0; //counts number of t's letters in current window
-        int low = 0, min_length = INT_MAX, min_start = 0;     
-        for(int high = 0; high<s.length(); high++) {
-            if(letters[s[high]] > 0) count++; //means that this letter is in t   
-            letters[s[high]]--; //reduce the count for the letter on which we are currently 
-            if(count == t.length()) { //if current windows contains all of the letters in t
-                while(low < high && letters[s[low]] < 0) letters[s[low]]++, low++; //move low ahead if its not of any significance
-                if(min_length > high-low) min_length = high-(min_start=low)+1; //update the min length
-                letters[s[low++]]++; //move low ahaead and also increment the value
-                count--; //count-- as we are moving low ahead & low pointed to a char in t before
+    string minWindow(string s, string t) {
+        unordered_map<char, int> T, W;
+
+        for(int i = 0; i < t.size(); i++)
+            T[t[i]] += 1;
+        
+        int have = 0, need = t.size();
+
+        int l = 0;
+
+        int rStart = -1, rLen = INT_MAX;
+
+        for(int r = 0; r < s.size(); r++){
+            char c = s[r];
+            W[c] += 1;
+
+            if(T.find(c) != T.end() && W[c] == T[c])
+                have += 1;
+            
+            while(have == need){
+                if((r - l + 1) < rLen){
+                    rLen = r - l + 1;
+                    rStart = l;
+                }
+
+                W[s[l]]--;
+
+                if(T.find(s[l]) != T.end() && W[s[l]] < T[s[l]])
+                    have -= 1;
+                
+                l++;
             }
         }
-        return min_length == INT_MAX ? "" : s.substr(min_start, min_length); //check for edge case & return the result
+        return rLen == INT_MAX ? "" : s.substr(rStart, rLen);
     }
 };
