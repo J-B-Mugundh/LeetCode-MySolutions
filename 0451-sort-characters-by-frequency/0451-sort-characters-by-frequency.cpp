f56273dcd742efe8c3ff67eb2337359d765e417d
Mugundh J B <mugundhjb@gmail.com>
2024-02-05 16:52:07 +0530

    Time: 151 ms (5.15%), Space: 11.4 MB (7.88%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
-    string frequencySort(string s) {
-    //  Create a hash map to store the frequency of each character
-    unordered_map<char, int> char_freq;
-    for (char c : s) {
-        char_freq[c]++;
+    static bool compare(char a, char b, unordered_map<char, int>& char_freq) {
+            if (char_freq[a] != char_freq[b]) {
+                return char_freq[a] > char_freq[b];
+            } else {
+                return a < b;
+            }
     }
-    
-    // Sort the characters based on their frequencies in decreasing order using lambda function
-    sort(s.begin(), s.end(), [&](char a, char b) {
-        if (char_freq[a] != char_freq[b]) {
-            return char_freq[a] > char_freq[b];
-        } else {
-            return a < b;
+
+    string frequencySort(string s) {
+        // Create a hash map to store the frequency of each character
+        unordered_map<char, int> char_freq;
+        for (char c : s) {
+            char_freq[c]++;
         }
-    });
-    
-    return s;
+
+        // Sort the characters based on their frequencies in decreasing order using compare function
+        sort(s.begin(), s.end(), [&](char a, char b) {
+            return compare(a, b, char_freq);
+        });
+
+        return s;
     }
 };
