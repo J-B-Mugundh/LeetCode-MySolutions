70702b0610420b918ba928223177340065ca30ce
Mugundh J B <mugundhjb@gmail.com>
2024-05-03 21:56:16 +0530

    Time: 0 ms (100%), Space: 7.3 MB (93.86%) - LeetHub

--- 0165-compare-version-numbers/0165-compare-version-numbers.cpp
+++ 0165-compare-version-numbers/0165-compare-version-numbers.cpp
@@ -1,34 +1,25 @@
 class Solution {
 public:
-    int compareVersion(string v1, string v2) {
-        int p1 = 0, p2 = 0;
-
-        while (p1 < v1.size() || p2 < v2.size()) {
-            int num1 = 0, num2 = 0;
-
-            // Parse the next revision in v1
-            while (p1 < v1.size() && v1[p1] != '.') {
-                num1 = num1 * 10 + (v1[p1] - '0');
-                p1++;
+    int compareVersion(string s1, string s2) {
+        int i = 0, j = 0, n = s1.length(), m = s2.length();
+        while(i < n || j < m){
+            int v1 = 0, v2 = 0;
+            while(i < n && s1[i] != '.'){
+                v1 = v1 * 10 + (s1[i] - '0');
+                i++;
             }
-
-            // Parse the next revision in v2
-            while (p2 < v2.size() && v2[p2] != '.') {
-                num2 = num2 * 10 + (v2[p2] - '0');
-                p2++;
+            while(j < m && s2[j] != '.'){
+                v2 = v2 * 10 + (s2[j] - '0');
+                j++;
             }
-
-            // Compare the two revisions
-            if (num1 > num2)
+            if(v1 > v2)
                 return 1;
-            else if (num1 < num2)
+            else if(v1 < v2)
                 return -1;
-
-            // Move to the next character after '.'
-            p1++;
-            p2++;
+            
+            i++;
+            j++;
         }
-
         return 0;
     }
 };
