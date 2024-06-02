f35763fa53023e518275b9e4a8503a0f4aa91001
Mugundh J B <mugundhjb@gmail.com>
2024-06-02 13:21:16 +0530

    Time: 5 ms (15.28%), Space: 9.9 MB (11.08%) - LeetHub

--- 1833-find-the-highest-altitude/1833-find-the-highest-altitude.cpp
+++ 1833-find-the-highest-altitude/1833-find-the-highest-altitude.cpp
@@ -1,11 +1,19 @@
 class Solution {
 public:
     int largestAltitude(vector<int>& gain) {
-        int sum = 0, maxi = 0;
-        for(int n : gain){
-            sum += n;
-            maxi = max(sum, maxi);
+        int maxi = 0, n = gain.size();
+        vector<int> alt(n + 1, 0);
+        for(int i = 0; i < n; i++){
+            alt[i + 1] = alt[i] + gain[i];
+            maxi = max(alt[i + 1], maxi);
         }
         return maxi;
+
+        // alt[i-1] + gain[i]
+        // 0, -5 => -5 
+        // -5, 1 => -4
+        // -4, 5 => 1
+        // 1, 0 => 1
+        // 1, -7 => -6
     }
 };
