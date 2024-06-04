0dfa1aee8368c458408460f647f528bebc1ce530
Mugundh J B <mugundhjb@gmail.com>
2024-06-04 22:16:10 +0530

    Time: 6 ms (16.33%), Space: 9 MB (26.04%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.cpp
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
     int longestPalindrome(string s) {
-        unordered_map<int, int> mp;
+        unordered_map<char, int> mp;
         for(char c : s)
             mp[c]++;
-        bool flag = false;
-        int len = 0, maxOdd = 0;
+
+        int len = 0, minOdd = INT_MAX;
         for(auto it : mp){
             if(it.second % 2 == 0)
                 len += it.second;
             else{
-                maxOdd = max(maxOdd, it.second);
+                len += it.second - 1;
+                minOdd = min(minOdd, it.second);
             }
         }
-        return len + maxOdd;
+
+        if (minOdd < INT_MAX)
+            return len+1;
+        return len;
     }
 };
