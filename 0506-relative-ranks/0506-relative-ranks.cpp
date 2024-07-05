6ee20018e1139495784179ebe00bf7812a1f0eb5
Mugundh J B <mugundhjb@gmail.com>
2024-07-05 21:34:35 +0530

    Time: 3 ms (96.7%), Space: 13.7 MB (80.59%) - LeetHub

--- 0506-relative-ranks/0506-relative-ranks.cpp
+++ 0506-relative-ranks/0506-relative-ranks.cpp
@@ -1,41 +1,30 @@
 class Solution {
-private:
-    int findMax(vector<int>& score) {
-        int maxScore = 0;
-        for (int s : score) {
-            if (s > maxScore) {
-                maxScore = s;
-            }
-        }
-        return maxScore;
-    }
 public:
     vector<string> findRelativeRanks(vector<int>& score) {
         int N = score.size();
 
-        // Add the original index of each score to the array
-        // Where the score is the key
-        int M = findMax(score);
-        vector<int> scoreToIndex(M + 1, 0);
+        // Create a max heap of pairs (score, index)
+        priority_queue<pair<int, int>> heap;
         for (int i = 0; i < N; i++) {
-            scoreToIndex[score[i]] = i + 1;
+            heap.push({score[i], i});
         }
-
-        const vector<string> MEDALS = {"Gold Medal", "Silver Medal", "Bronze Medal"};
 
         // Assign ranks to athletes
         vector<string> rank(N);
         int place = 1;
-        for (int i = M; i >= 0; i--) {
-            if (scoreToIndex[i] != 0) {
-                int originalIndex = scoreToIndex[i] - 1;
-                if (place < 4) {
-                    rank[originalIndex] = MEDALS[place - 1];
-                } else {
-                    rank[originalIndex] = to_string(place);
-                }
-                place++;
+        while (!heap.empty()) {
+            int originalIndex = heap.top().second;
+            heap.pop();
+            if (place == 1) {
+                rank[originalIndex] = "Gold Medal";
+            } else if (place == 2) {
+                rank[originalIndex] = "Silver Medal";
+            } else if (place == 3) {
+                rank[originalIndex] = "Bronze Medal";
+            } else {
+                rank[originalIndex] = to_string(place);
             }
+            place++;
         }
         return rank;
     }
