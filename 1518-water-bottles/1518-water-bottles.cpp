09f4678766bb1fec90e4517d90a1e3a734c8b4de
Mugundh J B <mugundhjb@gmail.com>
2024-07-07 22:17:58 +0530

    Time: 0 ms (100%), Space: 7 MB (72.3%) - LeetHub

--- 1518-water-bottles/1518-water-bottles.cpp
+++ 1518-water-bottles/1518-water-bottles.cpp
@@ -3,15 +3,14 @@ public:
     int numWaterBottles(int numBottles, int numExchange) {
         int cnt = 0, sum = 0;
         while(numBottles){
-            if(cnt == numExchange){
-                numBottles += 1;
-                cnt = 0;
-            } 
-            numBottles -= 1;
-            cnt++;
-            sum++;
+            if(numBottles < numExchange){
+                break;
+            }
+            numBottles -= numExchange;
+            sum += numExchange;
+            numBottles += 1;
         }
-        if(cnt == numExchange) sum++;
+        if(numBottles > 0) sum += numBottles;
         return sum;
     }
 };
