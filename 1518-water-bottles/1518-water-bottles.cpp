b9edf7f8e46e6769762b72bd336d56bcc4e4ec60
Mugundh J B <mugundhjb@gmail.com>
2024-07-07 22:39:26 +0530

    Time: 0 ms (100%), Space: 7 MB (72.3%) - LeetHub

--- 1518-water-bottles/1518-water-bottles.cpp
+++ 1518-water-bottles/1518-water-bottles.cpp
@@ -1,16 +1,6 @@
 class Solution {
 public:
     int numWaterBottles(int numBottles, int numExchange) {
-        int cnt = 0, sum = 0;
-        while(numBottles){
-            if(numBottles < numExchange){
-                break;
-            }
-            numBottles -= numExchange;
-            sum += numExchange;
-            numBottles += 1;
-        }
-        if(numBottles > 0) sum += numBottles;
-        return sum;
+        return numBottles + (numBottles - 1) / (numExchange - 1);
     }
 };
