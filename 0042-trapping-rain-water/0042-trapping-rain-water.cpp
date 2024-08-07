086a7654c6c51a262cea138416d193f78b28598c
Mugundh J B <mugundhjb@gmail.com>
2024-08-07 05:42:36 +0530

    Time: 7 ms (87.2%), Space: 24.8 MB (5.54%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,68 +1,30 @@
 class Solution {
 public:
-    // // Better approach - T.C => O(n) , S.C => O(n)
-    // int trap(vector<int>& height) {
-    //     int n = height.size();
-    //     if (n <= 2) return 0;
-
-    //     vector<int> leftMax(n), rightMax(n);
-    //     int maxSoFar = height[0];
-
-    //     // Compute leftMax
-    //     for (int i = 1; i < n; i++) {
-    //         leftMax[i] = maxSoFar;
-    //         maxSoFar = max(maxSoFar, height[i]);
-    //     }
-
-    //     maxSoFar = height[n - 1];
-
-    //     // Compute rightMax
-    //     for (int i = n - 2; i >= 0; i--) {
-    //         rightMax[i] = maxSoFar;
-    //         maxSoFar = max(maxSoFar, height[i]);
-    //     }
-
-    //     int trappedWater = 0;
-
-    //     // Calculate trapped water for each position
-    //     for (int i = 1; i < n - 1; i++) {
-    //         int minHeight = min(leftMax[i], rightMax[i]);
-    //         if (minHeight > height[i]) {
-    //             trappedWater += minHeight - height[i];
-    //         }
-    //     }
-
-    //     return trappedWater;
-    // }
-
-    // Optimal approach => T.C = O(n), S.C = O(1)
     int trap(vector<int>& height) {
-        int n = height.size();
-        int left = 0, right = n - 1;
-        int trappedWater = 0, maxLeft = 0, maxRight = 0;
+    int n = height.size();
+    if (n <= 2) return 0;
 
-        while(left <= right){
-            if(height[left] <= height[right]) // This condition makes sure that, in right half, there is someone >= height[left]
-            {
-                if(height[left] >= maxLeft)
-                    maxLeft = height[left];
-                else 
-                    trappedWater += maxLeft - height[left];
+    int trappedWater = 0, leftMax = 0, rightMax = 0;
+    int left = 0, right = n - 1;
+    while(left <= right){
+        if(height[left] <= height[right]){
+            if(height[left] > leftMax)
+                leftMax = height[left];
+            else
+                trappedWater += (leftMax - height[left]);
+            
+            left++;
+        }else{
+            if(height[right] > rightMax)
+                rightMax = height[right];
+            else
+                trappedWater += (rightMax - height[right]);
 
-                left++;
-            }
-            else // This condition makes sure that, in left half, there is someone >= height[right]
-            {
-                if(height[right] >= maxRight)
-                    maxRight = height[right];
-                else
-                    trappedWater += maxRight - height[right];
-                
-                right--;
-            }
+            right--;
         }
-        return trappedWater;
     }
 
+    return trappedWater;
+}
 
 };
