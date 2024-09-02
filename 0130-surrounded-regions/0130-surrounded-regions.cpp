885b27c01ff7eb82a832edf08b304babe489e7a9
Mugundh J B <mugundhjb@gmail.com>
2024-09-02 21:18:15 +0530

    Time: 9 ms (70.86%), Space: 13.6 MB (96.72%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -4,17 +4,24 @@ private:
         if(r < 0 || c < 0 || r >= n || c >= m || board[r][c] != 'O')
             return ;
         
-        board[r][c] = 'T';
-        dfs(r - 1, c, n, m, board);
-        dfs(r + 1, c, n, m, board);
-        dfs(r, c - 1, n, m, board);
-        dfs(r, c + 1, n, m, board);
+        board[r][c] = '*';
+        int dr[] = {1, 0, 0, -1};
+        int dc[] = {0, 1, -1, 0};
+
+        for (int k = 0; k < 4; k++) {
+            int nr = r + dr[k];
+            int nc = c + dc[k];
+            dfs(nr, nc, n, m, board);
+        }
+
     }
 
 public:
     void solve(vector<vector<char>>& board) {
         int n = board.size(), m = board[0].size();
-        // Capture the unsurrounded regions (O -> T)
+        // The idea here is if 'O' is gonna be in boundary, then it's not surrounded. So, we will mark all the O's that are in boundary or can be traversed by an O from the boundary
+
+        // Capture the unsurrounded regions (O -> *)
         for(int i = 0; i < n; i++){
             for(int j = 0; j < m; j++){
                 if(board[i][j] == 'O' && (i == 0 || i == n - 1 || j == 0 || j == m - 1))
@@ -29,6 +36,20 @@ public:
                     board[i][j] = 'X';
             }
         }
-        
+        // Capture the surrounded regions (O -> X)
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(board[i][j] == 'O')
+                    board[i][j] = 'X';
+            }
+        }
+
+        // Un capture the unsurrounded regions (T -> O)
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(board[i][j] == '*')
+                    board[i][j] = 'O';
+            }
+        }
     }
 };
