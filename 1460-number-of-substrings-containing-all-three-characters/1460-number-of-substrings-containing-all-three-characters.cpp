110374b1fb3a2702393bfe06370a0a6b4652f138
Mugundh J B <mugundhjb@gmail.com>
2024-09-09 19:03:02 +0530

    Time: 15 ms (77.98%), Space: 10 MB (43.44%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
@@ -6,7 +6,7 @@ public:
 
         for(int i = 0; i < s.size(); i++){
             lastSeen[s[i] - 'a'] = i;
-            if(lastSeen[0] != -1 && lastSeen[1] != -1 && lastSeen[2] != -1){
+            if(lastSeen[0] != -1 && lastSeen[1] != -1 && lastSeen[2] != -1){ // If this condition satisfies, then the substrings with characters prev to this particular string's start or min index can also form valid substring
                 cnt += (1 + min(lastSeen[0], min(lastSeen[1], lastSeen[2])));
             }
         }
