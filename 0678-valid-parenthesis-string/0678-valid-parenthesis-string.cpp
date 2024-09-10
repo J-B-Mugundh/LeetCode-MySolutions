0400321d0243c8ec24f79864720d9d79f346866a
Mugundh J B <mugundhjb@gmail.com>
2024-09-10 23:23:13 +0530

    Time: 3 ms (51.64%), Space: 7.8 MB (28.46%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -1,21 +1,35 @@
 class Solution {
-public:
+public: 
     bool checkValidString(string s) {
-        int leftMin = 0, leftMax = 0, n = s.length(), i = 0;
+        stack<int> openBrackets;
+        stack<int> asterisks;
 
-        for(char c : s){
-            if(c == '(')
-                leftMin++, leftMax++;
-            else if(c == ')')
-                leftMin--, leftMax--;
-            else if(c == '*')
-                leftMin--, leftMax++;
-            
-            if(leftMax < 0) 
-                return false;
-            if(leftMin < 0) 
-                leftMin = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s[i];
+
+            if (ch == '(') {
+                openBrackets.push(i);
+            } else if (ch == '*') {
+                asterisks.push(i);
+            } else {
+                if (!openBrackets.empty()) {
+                    openBrackets.pop();
+                } else if (!asterisks.empty()) {
+                    asterisks.pop();
+                } else {
+                    return false;
+                }
+            }
         }
-        return leftMin == 0;
+
+        while (!openBrackets.empty() && !asterisks.empty()) {
+            if (openBrackets.top() > asterisks.top()) {
+                return false;
+            }
+            openBrackets.pop();
+            asterisks.pop();
+        }
+
+        return openBrackets.empty();
     }
 };
